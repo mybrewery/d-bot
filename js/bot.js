c37e3fe82cd5505f1e59ab48ef3f271995f65d00
Marco Polo <Marco Polo>
2017-03-16 01:06:24 +0200

    bot: local-storage-based memory.

--- js/bot.js
+++ js/bot.js
@@ -2,12 +2,36 @@
 var Bot = function(){
 	this.timeoutID;
 	this.mind = window.mind;
-	this.mind.size = this.mind.length;
+	this.extraMind = this.load() || [];
 
 	this.say('Привет');
 };
 
 Bot.prototype = {
+	save : function(){
+		if (localStorage){
+			localStorage.setItem('dummy-bot-extramind', JSON.stringify(this.extraMind));
+		}
+	},
+	load : function(){
+		var data;
+
+		if (localStorage){
+			data = localStorage.getItem('dummy-bot-extramind');
+		}
+
+		if (data){
+			data = JSON.parse(data);
+			this.mind = this.mind.concat(data);
+		}
+
+		return data;
+	},
+	remember : function(text){
+		this.mind.push(text);
+		this.extraMind.push(text);
+		this.save();
+	},
 	think : function(secs){
 		return (Math.random() * secs + secs) * 1000;
 	},
@@ -20,14 +44,13 @@ Bot.prototype = {
 
 		split.size = split.length;
 
-
-		for (var a = 0; a < this.mind.size; a++){
+		for (var a = 0, l = this.mind.length; a < l; a++){
 			for (var b = 0; b < split.size; b++){
-				if (split[b].length < 3){
+				if (split[b].length < 4){
 					continue;
 				}
 
-				lowString = split[b].substring(0, 3);
+				lowString = split[b].substring(0, 4);
 				capString = lowString.charAt(0).toUpperCase() + lowString.slice(1);
 
 				if (this.mind[a].indexOf(lowString) > -1 || this.mind[a].indexOf(capString) > -1){
@@ -40,7 +63,11 @@ Bot.prototype = {
 		if (vars.length && Math.random() > 0.25){
 			id = vars[Math.floor(Math.random() * (vars.length - 1))];
 		} else {
-			id = Math.floor(Math.random() * (this.mind.size - 1));
+			id = Math.floor(Math.random() * (this.mind.length - 1));
+		}
+
+		if (split.length > 1 && !(/(.)\1\1/.test(text)) && this.mind.indexOf(text) < 0){
+			this.remember(text);
 		}
 
 		return this.mind[id];
