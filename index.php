0ba8e0c15f5878802dbe902b6d6c4ceca1d01190
Marco Polo <Marco Polo>
2017-03-18 01:57:51 +0200

    mind updated

--- index.php
+++ index.php
@@ -7,13 +7,46 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
+	<div id="bg" class="theme-sensitive">
+		<img class="theme-bg t0", src="./graphics/bg0.png">
+		<img class="theme-bg t1", src="./graphics/bg1.png">
+		<img class="theme-bg t2", src="./graphics/bg2.png">
+		<img class="theme-bg t3", src="./graphics/bg3.png">
+		<img class="theme-bg t4", src="./graphics/bg4.png">
+		<img class="theme-bg t5", src="./graphics/bg5.png">
+	</div>
 	<div id="history" class="chat">
 		<div id="history-content">
 			<!-- content -->
 		</div>
 	</div>
 	<div id="field" class="chat">
-		<textarea id="user-input"rows="3"></textarea>
+		<form><textarea id="user-input"rows="3"></textarea></form>
+	</div>
+	<div id="menu">
+		<table id='themes'>
+			<tr>
+				<th>THEME</th>
+			</tr>
+			<tr>
+				<td class="theme t0" data-theme="0"><div></div></td>
+			</tr>
+			<tr>	
+				<td class="theme t1" data-theme="1"><div></div></td>
+			</tr>
+			<tr>	
+				<td class="theme t2" data-theme="2"><div></div></td>
+			</tr>
+			<tr>
+				<td class="theme t3" data-theme="3"><div></div></td>
+			</tr>
+			<tr>	
+				<td class="theme t4" data-theme="4"><div></div></td>
+			</tr>	
+			<tr>	
+				<td class="theme t5" data-theme="5"><div></div></td>
+			</tr>	
+		</table>
 	</div>
 	<script type="text/javascript" src="./js/mind.js"></script>
 	<script type="text/javascript" src="./js/bot.js"></script>
